de60adfb2f245842f05f76fd5d7548d72d948941
ArielBarros <arielbarros12@gmail.com>
2016-06-20 17:31:42 -0300

    Stub da classe UsuarioXStream

---
+++ src/Persistence/UsuarioXstream.java
@@ -0,0 +1,29 @@
+package Persistence;
+
+import Profile.Perfil;
+
+public class UsuarioXstream implements IRepositorioUsuario {
+
+	public UsuarioXstream() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void cadastrar(Perfil usuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void buscar(String usuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void atualizar(Perfil usuario) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
